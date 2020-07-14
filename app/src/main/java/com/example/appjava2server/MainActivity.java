ee8bb35f5e1b705d1a055408cf291f95661f420f
dotkichspd11 <hiimmax99@gmail.com>
2020-07-14 08:49:24 +0700

    AppManage

--- app/src/main/java/com/example/appjava2server/MainActivity.java
+++ app/src/main/java/com/example/appjava2server/MainActivity.java
@@ -24,9 +24,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnSignIn = (Button)findViewById(R.id.btnSignIn);
-        txtSlogan = (TextView)findViewById(R.id.txtSlogan);
-        Typeface face  = Typeface.createFromAsset(getAssets(),"fonts/NABILA.TTF");
-        txtSlogan.setTypeface(face);
+//        txtSlogan = (TextView)findViewById(R.id.txtSlogan);
+//        Typeface face  = Typeface.createFromAsset(getAssets(),"fonts/NABILA.TTF");
+//        txtSlogan.setTypeface(face);
 
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
